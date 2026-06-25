4555b9bf2e79238b1a1407e06f10141f01965619
Everett Rike <everettrike@gmail.com>
2026-06-25 17:04:16 -0600

    Brought in all the correct data to display on the map

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ st.sidebar.header("ML Engine")
 model_choice = st.sidebar.selectbox("Select Model", ["Random Forest", "Gradient Descent"])
 st.sidebar.divider()
 
+
 # -- Model Specfics --
 
 #Make sure the model names match the filenames saved in directory
@@ -46,3 +47,18 @@ data = load_data()
 if model is None or data is None:
     st.error("Model or Data could not be loaded.")
     st.stop()  
+
+#Allow user to select a date to see fire risk
+st.sidebar.header("Date Selection")
+available_dates = data['date'].unique()
+selected_date = st.sidebar.selectbox("Select Date", available_dates)
+day_df = data[data['date'] == selected_date].copy()
+
+# -- Data Preprocessing --
+#Drop the same columns that were dropped during model training
+col_drop = ['fire','system:index','date', '.geo', 'T21_max', 'T21_mean', 'T21_stdDev']
+X_test = day_df.drop(columns=col_drop, errors='ignore')
+
+#Grab the models predictions for the selected date and add them to the dataframe as a new column
+probabilities = model.predict_proba(X_test)
+day_df['danger_score'] = probabilities[:, 1] * 100
